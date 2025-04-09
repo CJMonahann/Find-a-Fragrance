198af9105de13a53a5a027c0ba65b76ad8aa63af
Cornelius Monahan <corneliusmonahann@gmail.com>
2025-04-09 12:44:32 -0400

    created database schema: table relations, SQL connections with app

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,27 @@
 from flask import Flask
 
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+from os import environ
+import os
+
+# force loading of environment variables
+load_dotenv()
+
+DB_NAME = os.getenv("__SQLITE_DB")
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = os.getenv("__SECRET_KEY")
+
+DB_CONFIG_STR = 'sqlite:///' + os.path.join(basedir, DB_NAME)
+app.config['SQLALCHEMY_DATABASE_URI'] = DB_CONFIG_STR
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]= True
+
+# Create database connection and associate it with the Flask application
+db = SQLAlchemy(app)
+
 # Add models
-from app import routes
+from app import routes, models
